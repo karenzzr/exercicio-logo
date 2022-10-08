ab2aaa4ddfc25104e9b9a26b291d21810d0ee4fa
Karen Helena <helenakaren55@gmail.com>
2022-10-07 23:26:59 -0300

    first commit

---
+++ arquivo.js
@@ -0,0 +1,37 @@
+function logar() {
+
+    var email = document.getElementById("email").Value;
+    var senha = document.getElementById("senha").Value;
+
+
+
+    if (email.toLowercase() === "basesocial@gmail.com" && senha === "1234") {
+        alert("deu certo");
+
+    } else {
+        alert("deu errado")
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
